73ff7ac44fa83fe5dacea776b70d2fbc08e2a513
anryl050 <118693401+anryl050@users.noreply.github.com>
2023-03-19 10:02:54 -0700

    worked with BCS to solve the issue with definitng proper path for the file directory; used mini project as an example to add the Delete callback function; added comments throughout the code.

--- server.js
+++ server.js
@@ -19,52 +19,59 @@ app.use(clog);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+// callback function that gets executed when a client makes a GET request to Home page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page
+// callback function that gets executed when a client makes a GET request to Notes Page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route to a specific note
-app.get('api/notes', (req, res) =>{
-    fs.readFile(path.join(__dirname, "./db/db.json", 'utf8'), (err, data) => {
+// callback function that gets executed when a client makes a GET request to a specific note
+app.get('/api/notes', (req, res) =>{
+  // method to read the contents from db.json file in the "./bd/" directory.
+    fs.readFile("./db/db.json", (err, data) => {
         if (err) {
           console.error(err)}
+        //when the file is read, the contents are resturned as a JSON response, and then they are parsed to convert data to JS object. 
         res.json(JSON.parse(data));
         });
 });
 
-// POST route for a new note
-app.post('api/notes', (req, res) =>{
-    console.log(req.body);
-
+// callback function that gets executed when a client makes a POST request to crete a new note
+app.post('/api/notes', (req, res) =>{
+    //deconstructing the req.body object
     const {title, text} = req.body;
 
     if (req.body){
+        //creating a new Note object
         const newNote = {
             title,
             text
         };
+        // assigning a unique id property to the newNote object
         newNote.id = uuidv4();
 
-        fs.readFile(path.join(__dirname, "./db/db.json", 'utf8'), (err, data) => {
+        // method to read the contents from db.json file in the "./bd/" directory.
+        fs.readFile("./db/db.json", (err, data) => {
             if (err) {
               console.error(err);
             } 
 
+            //parsing the JSON response into the JS object, then parseData array is modified to include the newNote object. 
             const parsedData = JSON.parse(data);
             parsedData.push(newNote);
 
-            fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(parsedData, null, 2), (err) => {
+            // method to write the contents from updated pasreData array into the "./bd/" directory.
+            fs.writeFile("./db/db.json", JSON.stringify(parsedData, null, 2), (err) => {
                 if (err) {
                     console.error(err);
                   } 
                 })
-        
+
+        // JSON response to the client with data from the newNote object. 
         res.json(newNote);
         })
         } else {
@@ -72,7 +79,31 @@ app.post('api/notes', (req, res) =>{
         }
     });
 
+// callback function that gets executed when a client makes a DELETE request to a remove a specific note
+app.delete('/api/notes/:id', (req, res) => {
 
+  // retrieves the parameters from the URL of HTTPS request. Based on this, server is able to identify the note that user wants to delete.
+  const noteId = req.params.id;
+
+  // method to read the contents from db.json file in the "./bd/" directory.
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+
+    var parsedData = JSON.parse(data);
+    //filter method is used to create a new array to contain only elements of the original array that meet the specified criteria.
+    parsedData = parsedData.filter((note) => note.id !== noteId);
+
+    // method to write the contents from updated pasreData array into the "./bd/" directory.
+    fs.writeFile("./db/db.json", JSON.stringify(parsedData, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+      } 
+      res.json(`Item has been deleted 🗑️`);
+     });
+    });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
